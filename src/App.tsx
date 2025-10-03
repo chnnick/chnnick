00f048f0a337b2331a9cb489bc652811ae7a6a5d
Nicholas Chen <nickchenschool@gmail.com>
2025-10-03 13:45:42 -0400

    personal website stuff + hosting for github pages
    hopefully this works

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Terminal } from './components/Terminal';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-green-400 font-mono overflow-hidden">
+      <Terminal />
+    </div>
+  );
+}
+
+export default App;
